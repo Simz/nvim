73f1a44a583d066c83f0084b0cf832d90be246c9
Simon Ouellet <simon.ouellet@toumoro.com>
2023-04-18 17:11:33 +0000

    toggle term

--- lua/localfn.lua
+++ lua/localfn.lua
@@ -4,7 +4,7 @@ function M.docker_logs()
     --vim.api.nvim_command("call floaterm#run('new',1,[],'docker ps')")
     local cmd = {}
     cmd = 'ls -lah'
-    vim.api.nvim_command('FloatermNew!' .. cmd)
+    vim.api.nvim_command('FloatermToggle!' .. cmd)
   --  vim.api.nvim_call_function("floaterm#new",{true, cmd, fnarg, fnarg})
 end
 return M
