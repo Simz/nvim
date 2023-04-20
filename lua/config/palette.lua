bfdf7c09b12a71c0da47de5020a41fa6514315da
Simz <ouellet.simon@gmail.com>
2023-04-20 12:40:51 -0400

    Create palette.lua

---
+++ lua/config/palette.lua
@@ -0,0 +1,21 @@
+local command_center = require("command_center")
+
+command_center.add({
+  {
+    desc = "Docker - docker-compose stop",
+    cmd = "<CMD>!docker-compose stop<CR>",
+  },  {
+    desc = "Docker - docker-compose up",
+    cmd = "<CMD>!docker-compose up<CR>",
+  },  {
+    desc = "CDK - cdk synth --all",
+    cmd = "<CMD>FloatermNew! cdk synth<CR>",
+  },  {
+    desc = "CDK - cdk deploy --all",
+    cmd = "<CMD>FloatermNew! cdk deploy --all<CR>",
+  },  {
+    desc = "reload",
+    cmd = "<CMD>source $MYVIMRC<CR>",
+  },
+})
+require('telescope').load_extension('command_center')
